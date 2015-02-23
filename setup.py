eec3ca1381ab891481c0fc1ce20f1cfd965bf834
hugo <hugo>
2015-02-23 10:59:31 -0500

    add blaze/into to setup.py reqs

--- setup.py
+++ setup.py
@@ -12,13 +12,19 @@ if len(sys.argv)>1 and sys.argv[1] == 'develop':
         f.write(path)
     print("develop mode, wrote path (%s) to (%s)" % (path, fname))
     sys.exit()
+
 from distutils.core import setup
+try:
+    import setuptools
+except:
+    pass
 import os
 import sys
-__version__ = (0, 2)
+__version__ = (0, 2, 1)
 setup(
     name = 'multiuserblazeserver',
     version = '.'.join([str(x) for x in __version__]),
+    install_requires=['blaze >= 0.7.2', 'into >= 0.2.1'],
     packages = ['mbs',
                 'mbs.scripts',
                 'mbs.tests',
